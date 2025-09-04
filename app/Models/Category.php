0f0ec4305be410aad179c9723a18083a03d8da3c
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2025-09-04 09:30:34 -0300

    feat: implements relationship many to many in book and category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function books():BelongsToMany
+    {
+       return $this->belongsToMany(Book::class); 
+    }
 }
