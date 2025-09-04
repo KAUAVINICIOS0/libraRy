86a19f5f7b921d00cb720d38a4edfd8f1b3391dc
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2025-09-04 09:26:27 -0300

    feat: implements relationship on book models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     /** @use HasFactory<\Database\Factories\BookFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'isbn',
+        'year_published',
+        'author_id',
+        'publisher_id',
+        'status_book',
+    ];
+
+    public function author():BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher():BelongsTo
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function categories():BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
