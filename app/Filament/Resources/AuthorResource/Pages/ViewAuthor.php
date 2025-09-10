2999a6f7143e5caddc900dc6d5723a6f79ee4a77
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2025-09-10 08:32:38 -0300

    feat: implements icon and outlined in buttons of category and author

--- app/Filament/Resources/AuthorResource/Pages/ViewAuthor.php
+++ app/Filament/Resources/AuthorResource/Pages/ViewAuthor.php
@@ -13,7 +13,9 @@ class ViewAuthor extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            Actions\EditAction::make()
+                ->icon('heroicon-s-pencil')
+                ->outlined(),
         ];
     }
 }
--- app/Filament/Resources/CategoryResource/Pages/ViewCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/ViewCategory.php
@@ -13,7 +13,9 @@ class ViewCategory extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            Actions\EditAction::make()
+                ->icon('heroicon-s-pencil')
+                ->outlined(),
         ];
     }
 }
